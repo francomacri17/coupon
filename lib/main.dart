f853ead4cbad5860e306375be09fad8edd34c340
Franco <francomacri17@gmail.com>
2020-04-23 20:54:36 -0300

    add splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:coupon_app/src/blocs/login.bloc.dart';
 import 'package:coupon_app/src/resources/blocProvider.dart';
-import 'package:coupon_app/src/resources/firebaseRepository.dart';
 import 'package:coupon_app/src/ui/home.page.dart';
 import 'package:coupon_app/src/ui/login.page.dart';
 import 'package:coupon_app/src/ui/signUp.page.dart';
+import 'package:coupon_app/src/ui/splash.page.dart';
 import 'package:coupon_app/src/userPreferences/userPreferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -26,6 +26,10 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: this.getCurrentUser() != null ? 'home' : 'login',
       routes: {
+        'splash': (BuildContext context) => BlocProvider<LoginBloc>(
+              bloc: LoginBloc(),
+              child: SplashPage(),
+            ),
         'login': (BuildContext context) => BlocProvider<LoginBloc>(
               bloc: LoginBloc(),
               child: LoginPage(),
