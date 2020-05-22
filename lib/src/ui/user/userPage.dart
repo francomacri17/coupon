7b16503341af3b7d0415d78fbc50b9fbc22b2721
Franco <francomacri17@gmail.com>
2020-05-22 18:51:42 -0300

    user page scroll listview items add

--- lib/src/ui/user/userPage.dart
+++ lib/src/ui/user/userPage.dart
@@ -2,7 +2,6 @@ import 'package:coupon_app/src/blocs/user/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UserPage extends StatelessWidget {
   final FirebaseUser _user;
@@ -16,18 +15,54 @@ class UserPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('User page')),
-        body: Container(
-          alignment: Alignment.topCenter,
-            child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CircleAvatar(
-            radius: 80,
-            backgroundColor: Colors.black,
-            backgroundImage: _user.photoUrl != null
-                ? NetworkImage(_user.photoUrl)
-                : NetworkImage(
-                    'https://i.ibb.co/3mPYm2N/Deafult-Profile-Pitcher.png'),
+        body: ListView(
+          children: <Widget>[
+          Container(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius: 80.0,
+                  backgroundColor: Colors.black,
+                  backgroundImage: _user.photoUrl != null
+                      ? NetworkImage(_user.photoUrl)
+                      : NetworkImage(
+                          'https://i.ibb.co/3mPYm2N/Deafult-Profile-Pitcher.png'),
+                ),
+              )),
+          SizedBox(
+            height: 10,
           ),
-        )));
+          Container(
+            color: Colors.blue,
+            width: double.infinity,
+            height: 100,
+          ),
+          Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 100,
+          ),
+          Container(
+            color: Colors.greenAccent,
+            width: double.infinity,
+            height: 100,
+          ),
+                    Container(
+            color: Colors.blue,
+            width: double.infinity,
+            height: 100,
+          ),
+                    Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 100,
+          ),
+                    Container(
+            color: Colors.blue,
+            width: double.infinity,
+            height: 100,
+          ),
+        ]));
   }
 }
